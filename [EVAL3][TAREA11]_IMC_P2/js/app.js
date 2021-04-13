49dea51de1df3b264f54dfe190215ee635188b8e
gpalex384 <agarc384@gmail.com>
2021-04-13 21:13:52 +0200

    error altura 0, cambio en formula IMC, comentar código

--- [EVAL3][TAREA11]_IMC_P2/js/app.js
+++ [EVAL3][TAREA11]_IMC_P2/js/app.js
@@ -7,10 +7,11 @@ function calcularImc(){
     var mensaje;
     var mensajeLabel;
     var err;
+
     // Capturamos los inputs
     nombre = document.getElementById("nombre");
     typeof(nombre);
-    if (!isNaN(parseFloat(nombre.value))) {
+    if (!isNaN(parseFloat(nombre.value)) || nombre.value == "") {
         nombre.value = "ComoTeLlames";
     }      
     edad = document.getElementById("edad");
@@ -27,18 +28,25 @@ function calcularImc(){
         imc.value = "ERROR"
         err=true;
     }
+
     // Realizamos el cálculo si el input está validado
     if (!err) {
-        resultado=(parseFloat(peso.value)/parseFloat(altura.value)).toFixed(2);
+        resultado=(parseFloat(peso.value)/parseFloat(altura.value^2)).toFixed(2);
         imc.value = resultado;
         mensaje = "Hola "+ nombre.value + ", tu IMC es de "+ imc.value;
     }
+    // Escribimos un mensaje de error si falla la validación de datos
     else {
         mensaje = "Hay algún error en los datos de entrada.";
+        if (altura.value == 0) {
+            mensaje += " La altura no puede ser 0."
+        }
     }
 
+    // Mostramos el mensaje en pantalla
     mensajeLabel.textContent=mensaje;
         
+    // Mostramos el mensaje por consola
     console.log(mensaje);
     return false;
 }
